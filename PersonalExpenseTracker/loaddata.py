8f36e194987b09f673125a5b4c279bc45a218da8
prasad1973 <prasad1999@gmail.com>
2024-12-08 20:36:57 -0600

    added prompting, fully functional

--- PersonalExpenseTracker/loaddata.py
+++ PersonalExpenseTracker/loaddata.py
@@ -5,22 +5,25 @@ class DataLoadAndStore:
     
     def load(self):
         ret = []
-        with open(self.__filepath, "r") as f:
-            add = {}
-            lines = f.readlines
-            for i in range(1, len(lines)):
-                fields = lines[i].split(',')
-                add['date'] = fields[0].strip()
-                add['category'] = fields[1].strip()
-                add['amount'] = fields[2].strip()
-                add['description'] = fields[3].strip()
-                ret.append(add)
+        try:
+            with open(self.__filepath, "r") as f:
+                add = {}
+                lines = f.readlines()
+                for i in range(1, len(lines)):
+                    fields = lines[i].split(',')
+                    add['date'] = fields[0].strip()
+                    add['category'] = fields[1].strip()
+                    add['amount'] = fields[2].strip()
+                    add['description'] = fields[3].strip()
+                    ret.append(add)
+                return ret
+        except FileNotFoundError:
             return ret
     
     def store(self, data):
-        with open(self.__filepath, "w") as f:
+        with open(self.__filepath, "w+") as f:
             write = []
-            write.append("Date,Category,Amount,Description")
+            write.append("Date,Category,Amount,Description\n")
             for entry in data:
-                write.append(f"{entry['date']},{entry['category']},{entry['amount']},{entry['description']}")
+                write.append(f"{entry['date']},{entry['category']},{entry['amount']},{entry['description']}\n")
             f.writelines(write)
--- PersonalExpenseTracker/personalexpensetracker.py
+++ PersonalExpenseTracker/personalexpensetracker.py
@@ -1,23 +1,24 @@
 from loaddata import DataLoadAndStore as DLS
 from datetime import datetime
+import os
 
 data = []
 budget = 0
 
 def addexpense():
-    amount = input('Please enter an amount:')
-    description = input('Please give a description:')
-    category = input('Please give a category:')
-    today_date = datetime.today()
-    date = today_date.strftime('%Y-%m-%d')
-    add = {}
-    add['date'] = date.strip()
-    add['category'] = category.strip()
+    amount = input('Please enter an amount: ')
     try:
         amount = int(amount.strip())
     except ValueError:
         print("Invalid input: not an integer, please retry adding expense.")
         return
+    description = input('Please give a description: ')
+    category = input('Please give a category: ')
+    today_date = datetime.today()
+    date = today_date.strftime('%Y-%m-%d')
+    add = {}
+    add['date'] = date.strip()
+    add['category'] = category.strip()
     add['amount'] = str(amount)
     add['description'] = description.strip()
     data.append(add)
@@ -34,7 +35,7 @@ def viewexpenses():
         print("-------------------------------------------------------")
 
 def setbudget():
-    budget = input('Please enter your desired budget:')
+    budget = input('Please enter your desired budget: ')
     try: 
         budget = int(budget.strip())
     except ValueError:
@@ -43,7 +44,7 @@ def setbudget():
     expenses = 0
     for entry in data:
         try:
-            expenses += int(entry.strip())
+            expenses += int(entry['amount'].strip())
         except ValueError:
             print("Invalid expense entry, please repair before attempting to track budget.")
             return
@@ -51,3 +52,37 @@ def setbudget():
         print(f"You are within budget, with ${str(budget - expenses)} to spare.")
     else:
         print(f"Warning, you are ${str(expenses - budget)} over your budget!")
+
+run = True
+filepath = os.getcwd() + "/PersonalExpenseTracker/budgetdata.csv"
+loader = DLS(filepath)
+data = loader.load()
+
+while run:
+    menu = "Select a number from 1 to 5 to perform an action:\n"
+    menu += "1: Add an expense\n"
+    menu += "2: View expenses\n"
+    menu += "3: Track budget\n"
+    menu += "4: Save expenses to file\n"
+    menu += "5: Save and quit\n"
+    menu += "-------------------------------------\n"
+    selection = input(menu)
+    try:
+        selection = int(selection.strip())
+        if selection < 6 and selection > 0:
+            if selection == 1:
+                addexpense()
+            elif selection == 2:
+                viewexpenses()
+            elif selection == 3:
+                setbudget()
+            elif selection == 4:
+                loader.store(data)
+            elif selection == 5:
+                loader.store(data)
+                print("Goodbye!")
+                run = False
+            else:
+                print("Unrecognized input, please try again")
+    except ValueError:
+        print("Unrecognized input, please try again")
